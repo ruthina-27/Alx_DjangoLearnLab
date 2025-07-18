d764a3ddbb9f46c1d984b8727e9342004bc3339b
Ruth Shimeles Getachew <ruthshimelesgetachew73@gmail.com>
2025-07-19 01:36:03 +0300

    do some corrections

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,15 +1,24 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-# Query all books by a specific author
-specific_author = Author.objects.get(name="Some Author")
-authors_books = Book.objects.filter(author=specific_author)
-print("Books by", specific_author.name, ":", list(authors_books))
+# Query all books by a specific author.
+def query_books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    books = Book.objects.filter(author=author)
+    print("Books by author:", [book.title for book in books])
 
-# List all books in a library
-library = Library.objects.get(name="Some Library")
-library_books = library.books.all()
-print("Books in", library.name, ":", list(library_books))
+# List all books in a library.
+def list_books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    books = library.books.all()
+    print("Books in library:", [book.title for book in books])
 
-# Retrieve the librarian for a library
-librarian = library.librarian  # OneToOneField reverse access
-print("Librarian for", library.name, ":", librarian.name) 
+# Retrieve the librarian for a library.
+def get_librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    librarian = library.librarian
+    print("Librarian for library:", librarian.name)
+
+# Example usage (uncomment and adjust names as needed):
+# query_books_by_author("Some Author")
+# list_books_in_library("Some Library")
+# get_librarian_for_library("Some Library") 
