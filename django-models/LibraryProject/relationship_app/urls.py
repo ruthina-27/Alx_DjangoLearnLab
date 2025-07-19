a309be77a57089e40d9f31a7323451d53b0f847e
Ruth Shimeles Getachew <ruthshimelesgetachew73@gmail.com>
2025-07-19 20:28:27 +0300

    fix checker issues

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import list_books, LibraryDetailView, LibraryBooksListView, register
+from . import views
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', register, name='register'),
-    path('books/', list_books, name='list_books'),  # Function-based view for listing books
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view for library details
-    path('library/<int:pk>/books/', LibraryBooksListView.as_view(), name='library_books_list'),  # Class-based ListView for books in a library
+    path('register/', views.register, name='register'),
+    path('books/', views.list_books, name='list_books'),  # Function-based view for listing books
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # Class-based view for library details
+    path('library/<int:pk>/books/', views.LibraryBooksListView.as_view(), name='library_books_list'),  # Class-based ListView for books in a library
 ] 
