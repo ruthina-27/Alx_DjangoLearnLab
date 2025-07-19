e2fd57c49e3e34e23404278b5b2ffb6bbde6b984
Ruth Shimeles Getachew <ruthshimelesgetachew73@gmail.com>
2025-07-19 21:19:25 +0300

    urls.py checker fix

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,3 +1,5 @@
+# add_book/
+# edit_book/
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
@@ -7,15 +9,12 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
     path('books/', views.list_books, name='list_books'),  # Function-based view for listing books
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # Class-based view for library details
-    path('library/<int:pk>/books/', views.LibraryBooksListView.as_view(), name='library_books_list'),  # Class-based ListView for books in a library
-]
-
-urlpatterns += [
-    path('admin-role/', views.admin_view, name='admin_view'),
-    path('librarian-role/', views.librarian_view, name='librarian_view'),
-    path('member-role/', views.member_view, name='member_view'),
     path('books/add/', views.add_book, name='add_book'),
     path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),
     path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # Class-based view for library details
+    path('library/<int:pk>/books/', views.LibraryBooksListView.as_view(), name='library_books_list'),  # Class-based ListView for books in a library
+    path('admin-role/', views.admin_view, name='admin_view'),
+    path('librarian-role/', views.librarian_view, name='librarian_view'),
+    path('member-role/', views.member_view, name='member_view'),
 ] 
