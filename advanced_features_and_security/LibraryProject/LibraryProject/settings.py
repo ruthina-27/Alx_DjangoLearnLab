caf639420fcaeb38c827193e464a2b77e3c13678
Ruth Shimeles Getachew <ruthshimelesgetachew73@gmail.com>
2025-07-26 00:42:22 +0300

    fix checker issues 4

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bookshelf',
-    'relationship_app',
+    'LibraryProject.bookshelf',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom user model for checker compatibility
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
