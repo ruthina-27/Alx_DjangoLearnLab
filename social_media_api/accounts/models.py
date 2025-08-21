5ef966ce62dd6410992a8eed44e5be9fdb46977b
Ruth shimeles <ruthshimelesgetachew73@gmail.com>
2025-08-21 22:58:39 +0300

    Fix profile_picture field to use ImageField and add Pillow dependency

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class User(AbstractUser):
     bio = models.TextField(blank=True)
-    profile_picture = models.URLField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
     # followers: users who follow this user
     followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
 
